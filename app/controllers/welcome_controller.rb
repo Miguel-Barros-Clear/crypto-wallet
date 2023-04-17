2cb5640cae62df7d66f8457acd039c619f661c69
Miguel-Barros-Clear <miguel.barros.clear@gmail.com>
2023-04-17 11:17:40 -0300

    "feat: cookies and sessions"
    This reverts commit c880139529804e4193b8e585a3624758343f1851.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,7 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    cookies[:curso] = "Curso de Ruby on Rails - Jackson Pires [Cookie]"
-    session[:curso] = "Curso de Ruby on Rails - Jackson Pires [Session]"
     @nome = "Jackson"
     @curso = "Rails"
   end
